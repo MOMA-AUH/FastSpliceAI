841a45542a6785331898feddbfa60e034d6cbfee
Jakob Jersild Nielsen <jjn@inguz.dev>
2026-08-05 09:49:06 +0200

    Ruff formatting

--- spliceai/__init__.py
+++ spliceai/__init__.py
@@ -1,8 +1,7 @@
 import signal
 from importlib.metadata import version
 
-
 signal.signal(signal.SIGINT, lambda x, y: exit(0))
 
-name = 'spliceai'
+name = "spliceai"
 __version__ = version(name)
--- tests/test_delta_score.py
+++ tests/test_delta_score.py
@@ -1,47 +1,43 @@
-from collections import namedtuple
 import unittest
+from collections import namedtuple
 from importlib.resources import files
-from tests import name
+
 from spliceai.utils import Annotator, get_delta_scores
+from tests import name
 
-
-Record = namedtuple('Record', ['chrom', 'pos', 'ref', 'alts'])
+Record = namedtuple("Record", ["chrom", "pos", "ref", "alts"])
 
 
 class TestDeltaScore(unittest.TestCase):
-
     @classmethod
     def setUpClass(cls):
-
-        fasta_path = files(name).joinpath('data/test.fa')
-        fasta_without_prefix_path = files(name).joinpath('data/test_without_prefix.fa')
-        cls.ann = Annotator(fasta_path, 'grch37')
-        cls.ann_without_prefix = Annotator(fasta_without_prefix_path, 'grch37')
+        fasta_path = files(name).joinpath("data/test.fa")
+        fasta_without_prefix_path = files(name).joinpath("data/test_without_prefix.fa")
+        cls.ann = Annotator(fasta_path, "grch37")
+        cls.ann_without_prefix = Annotator(fasta_without_prefix_path, "grch37")
 
     def test_get_delta_score_acceptor(self):
-
-        record = Record('10', 94077, 'A', ['C'])
+        record = Record("10", 94077, "A", ["C"])
         scores = get_delta_scores(record, self.ann, 500, 0)
-        self.assertEqual(scores, ['C|TUBB8|0.15|0.27|0.00|0.05|89|-23|-267|193'])
+        self.assertEqual(scores, ["C|TUBB8|0.15|0.27|0.00|0.05|89|-23|-267|193"])
         scores = get_delta_scores(record, self.ann_without_prefix, 500, 0)
-        self.assertEqual(scores, ['C|TUBB8|0.15|0.27|0.00|0.05|89|-23|-267|193'])
+        self.assertEqual(scores, ["C|TUBB8|0.15|0.27|0.00|0.05|89|-23|-267|193"])
 
-        record = Record('chr10', 94077, 'A', ['C'])
+        record = Record("chr10", 94077, "A", ["C"])
         scores = get_delta_scores(record, self.ann, 500, 0)
-        self.assertEqual(scores, ['C|TUBB8|0.15|0.27|0.00|0.05|89|-23|-267|193'])
+        self.assertEqual(scores, ["C|TUBB8|0.15|0.27|0.00|0.05|89|-23|-267|193"])
         scores = get_delta_scores(record, self.ann_without_prefix, 500, 0)
-        self.assertEqual(scores, ['C|TUBB8|0.15|0.27|0.00|0.05|89|-23|-267|193'])
+        self.assertEqual(scores, ["C|TUBB8|0.15|0.27|0.00|0.05|89|-23|-267|193"])
 
     def test_get_delta_score_donor(self):
-
-        record = Record('10', 94555, 'C', ['T'])
+        record = Record("10", 94555, "C", ["T"])
         scores = get_delta_scores(record, self.ann, 500, 0)
-        self.assertEqual(scores, ['T|TUBB8|0.01|0.18|0.15|0.62|-2|110|-190|0'])
+        self.assertEqual(scores, ["T|TUBB8|0.01|0.18|0.15|0.62|-2|110|-190|0"])
         scores = get_delta_scores(record, self.ann_without_prefix, 500, 0)
-        self.assertEqual(scores, ['T|TUBB8|0.01|0.18|0.15|0.62|-2|110|-190|0'])
+        self.assertEqual(scores, ["T|TUBB8|0.01|0.18|0.15|0.62|-2|110|-190|0"])
 
-        record = Record('chr10', 94555, 'C', ['T'])
+        record = Record("chr10", 94555, "C", ["T"])
         scores = get_delta_scores(record, self.ann, 500, 0)
-        self.assertEqual(scores, ['T|TUBB8|0.01|0.18|0.15|0.62|-2|110|-190|0'])
+        self.assertEqual(scores, ["T|TUBB8|0.01|0.18|0.15|0.62|-2|110|-190|0"])
         scores = get_delta_scores(record, self.ann_without_prefix, 500, 0)
-        self.assertEqual(scores, ['T|TUBB8|0.01|0.18|0.15|0.62|-2|110|-190|0'])
+        self.assertEqual(scores, ["T|TUBB8|0.01|0.18|0.15|0.62|-2|110|-190|0"])
