c3babd9f601a7feb857ad7209049e233d19b3d9c
Jakob Jersild Nielsen <jjn@inguz.dev>
2026-08-06 11:06:32 +0200

    Set logging level to INFO

--- spliceai/__init__.py
+++ spliceai/__init__.py
@@ -1,10 +1,12 @@
+import logging
 import signal
 import sys
 from importlib.metadata import version
-from logging import getLogger
 
 signal.signal(signal.SIGINT, lambda x, y: sys.exit(0))
 
 name = "spliceai"
-logger = getLogger(name)
 __version__ = version(name)
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(name)
